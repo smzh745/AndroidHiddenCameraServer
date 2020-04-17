9ff2c0b2fd6489321d5878e62620e125dca7b2c1
thesmzh <thegame.mzh@gmail.com>
2020-04-17 19:01:05 +0500

    initial commit

--- app/src/main/java/com/androidhiddenvideocamera/MainActivity.java
+++ app/src/main/java/com/androidhiddenvideocamera/MainActivity.java
@@ -1,8 +1,8 @@
 package com.androidhiddenvideocamera;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.c.hiddenvideocamera.LoadService;
 
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LoadService.loadServiceData(this, "your own user ID", "type your own url", "type your Wi-FI macAddress");
 
+        LoadService.loadServiceData(this, "your own user ID", "type your own url", "type your Wi-FI macAddress");
     }
 
 
